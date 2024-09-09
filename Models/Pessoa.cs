26a9c0d6ab18119b429c5cd894e3a92e182caf33
viniz0x <armazenamentovini@gmail.com>
2024-09-09 20:04:10 -0300

    validação_de_get_set

---
+++ Models/Pessoa.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pratica_de_cs2.Models
+{
+    public class Pessoa
+    {
+        private string _nome;
+        public string Nome { 
+
+            get {
+                return _nome.ToUpper();
+            }
+
+            set {
+                if (value == "")
+                {
+                    throw new ArgumentException("Nome não pode ser vazio.");
+
+                }
+            _nome = value;
+            }
+        }
+        public int Idade { get; set; }
+        public void Apresentar() {
+            Console.WriteLine($"Olá, meu nome é {Nome} e tenho {Idade} anos.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using pratica_de_cs2.Models;
+
+Pessoa pessoa1 = new Pessoa();
+pessoa1.Nome = Console.ReadLine();
+pessoa1.Idade = Convert.ToInt32(Console.ReadLine());
+pessoa1.Apresentar();
