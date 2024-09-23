97445b0a78aec70d8319d4bc5f789e0387816bca
viniz0x <armazenamentovini@gmail.com>
2024-09-23 15:51:45 -0300

    atributos

--- Models/Venda.cs
+++ Models/Venda.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace pratica_de_cs2.Models
 {
@@ -14,6 +15,8 @@ namespace pratica_de_cs2.Models
         //     DataVenda = dataVenda;
         // }
         public int Id { get; set; }
+
+        [JsonProperty("Nome_Produto")]
         public string Produto { get; set; }
         public decimal Preco { get; set; }
         public DateTime DataVenda { get; set; }
